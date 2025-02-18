bef86f0b6dfe0a600b4e8c506bfe47210e3c4dd5
RodionovaAP <rodionovaap@admlr.lipetsk.ru>
2025-02-18 20:59:24 +0300

    first commit

---
+++ src/main/java/org/example/generate_number/GenerateNumberApplication.java
@@ -0,0 +1,13 @@
+package org.example.generate_number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateNumberApplication.class, args);
+	}
+
+}
